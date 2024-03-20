a711c25ae6a26e5b1ea0c61081dbf6fa8487b019
Vaishali <kaushalvaishali1994@gmail.com>
2024-03-20 11:21:35 +0530

    push pending page

--- resources/views/nursery/list.blade.php
+++ resources/views/nursery/list.blade.php
@@ -92,14 +92,14 @@
                                                 class="btn btn-primary">View</a>
                                             <?php
 
-                                            // if(env('REG_LAST_DATE') < date('Y-m-d')){
+                                            if(env('REG_LAST_DATE') < date('Y-m-d')){
                                             if ($n['nursery_status']['approved_reject_by_dso'] == 0) { ?>
                                             <a href="{{url('dso/nursery/report/').'/'.$n['secure_id']}}"
                                                 class="btn btn-primary">Proceed</a>
 
                                             <?php  } 
 
-                                        // } 
+                                        } 
 
                                          ?>
 
